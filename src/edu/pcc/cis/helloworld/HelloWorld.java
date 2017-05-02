8087f752c7dac6fd48c668b815b9f715a3c9bc95
Matt Hansen <matt.hansen@pcc.edu>
2017-05-01 20:46:53 -0700

    added main method that prints greeting

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,4 +4,8 @@ package edu.pcc.cis.helloworld;
  * Created by Matt on 5/1/2017.
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Greetings, all.");
+        System.out.println("This will be interesting...");
+    }
 }
